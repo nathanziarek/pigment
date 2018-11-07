5498469d8203e9f9b96a3a667017e7648c03f5c3
Nathan Ziarek <nziarek@directs.com>
2018-11-07 15:48:44 -0600

    rgb-to-lab tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 const hexToRgb = require("../lib/hex-to-rgb");
 const hslToRgb = require("../lib/hsl-to-rgb");
 const labToRgb = require("../lib/lab-to-rgb");
+const rgbToLab = require("../lib/rgb-to-lab");
 
 const t_hexToRgb = hexToRgb("#336699"); //51,102,153
 const t_hslToRgb = hslToRgb({ h: 210, s: 0.5, l: 0.259 }); //33,66,99
 const t_labToRgb = labToRgb({ l: 42.00998577686647, a: -0.1447448120508732, b: -32.85151286536059 }); //33,66,99
+const t_rgbToLab = rgbToLab({ r: 51, g: 102, b: 153 }); //33,66,99
 
 console.log(
   "hexToRgb",
@@ -24,5 +26,12 @@ console.log(
     t_labToRgb.r == 51,
     t_labToRgb.g == 102,
     t_labToRgb.b == 153
+  );
+  
+  console.log(
+    "rgbToLab",
+    Math.round(t_rgbToLab.l) == 42,
+    Math.round(t_rgbToLab.a) == 0,
+    Math.round(t_rgbToLab.b) == -33
   );
   
