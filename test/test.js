8a3a0c2154e76000cfd74412b18805bddcc6fa96
Nathan Ziarek <nziarek@directs.com>
2018-11-07 15:41:41 -0600

    Fixing lab-to-rgb

--- test/test.js
+++ test/test.js
@@ -1,6 +1,28 @@
-var  hexToRgb  = require("../lib/hex-to-rgb");
-var  hslToRgb  = require("../lib/hsl-to-rgb");
+const hexToRgb = require("../lib/hex-to-rgb");
+const hslToRgb = require("../lib/hsl-to-rgb");
+const labToRgb = require("../lib/lab-to-rgb");
 
-console.log('hexToRgb', hexToRgb("#336699"), { r: 51, g: 102, b: 153 });
+const t_hexToRgb = hexToRgb("#336699"); //51,102,153
+const t_hslToRgb = hslToRgb({ h: 210, s: 0.5, l: 0.259 }); //33,66,99
+const t_labToRgb = labToRgb({ l: 42.00998577686647, a: -0.1447448120508732, b: -32.85151286536059 }); //33,66,99
 
-console.log('hslToRgb', hslToRgb({h:210,s:.50,l:.259}), { r: 33, g: 66, b: 99 });
+console.log(
+  "hexToRgb",
+  t_hexToRgb.r == 51,
+  t_hexToRgb.g == 102,
+  t_hexToRgb.b == 153
+);
+console.log(
+  "hslToRgb",
+  t_hslToRgb.r == 33,
+  t_hslToRgb.g == 66,
+  t_hslToRgb.b == 99 
+);
+
+console.log(
+    "labToRgb",
+    t_labToRgb.r == 51,
+    t_labToRgb.g == 102,
+    t_labToRgb.b == 153
+  );
+  
